2f6f4641fa34f7a91897a0397ddf2b455a832daf
1azyfox <timyr.xas@gmail.com>
2021-12-26 10:54:58 +0300

    Добавил класс Calculator с методами сложения, вычитания, умножения, деления, возведения в степень, нахождения n-го корня, вычисления логарифма по требуемому основанию. Добавил по нескольку тестов на каждый метод: для случаев int и double входных данных.

---
+++ src/main/java/com/sbrf/reboot/Calculator.java
@@ -0,0 +1,57 @@
+package com.sbrf.reboot;
+
+public class Calculator {
+
+    public <T extends Number> double getAddition(T firstNumber, T secondNumber) {
+        if (firstNumber.doubleValue() % 1 != 0 || secondNumber.doubleValue() % 1 != 0) {
+            return Double.sum(firstNumber.doubleValue(), secondNumber.doubleValue());
+        } else {
+            return Integer.sum(firstNumber.intValue(), secondNumber.intValue());
+        }
+    }
+
+    public <T extends Number> double getSubtraction(T firstNumber, T secondNumber) {
+        return firstNumber.doubleValue() - secondNumber.doubleValue();
+    }
+
+    public <T extends Number> double getMultiplication(T firstNumber, T secondNumber) {
+        return firstNumber.doubleValue() * secondNumber.doubleValue();
+    }
+
+    public <T extends Number> double getDivision(T firstNumber, T secondNumber) {
+        return firstNumber.doubleValue() / secondNumber.doubleValue();
+    }
+
+    public <T extends Number> double getExponentiation(T firstNumber, T power) {
+        if (firstNumber.doubleValue() % 1 != 0 || power.doubleValue() % 1 != 0) {
+            return Math.pow(firstNumber.doubleValue(), power.doubleValue());
+        } else {
+            int result = 1;
+            int numberOfMultiplications = power.intValue();
+            do {
+                result *= firstNumber.doubleValue();
+                numberOfMultiplications--;
+            } while (numberOfMultiplications != 0);
+            return result;
+        }
+    }
+
+    public <T extends Number> double getNthRoot(T number, T rootNumber) {
+        return Math.pow(number.doubleValue(), 1 / rootNumber.doubleValue());
+    }
+
+    public <T extends Number> double getLogarithm(T number, T base) {
+        double answer = Math.log(number.doubleValue()) / Math.log(base.doubleValue());
+        int numberOfDigitsInAnswer = Double.toString(answer).length() - 1;
+        int correctNumberOfDigits = numberOfDigitsInAnswer - 2;
+        double exactAnswer = (Math.round(answer * Math.pow(10, correctNumberOfDigits))) / (Math.pow(10, correctNumberOfDigits));
+        double plus = answer + 0.000000000000001;
+        double minus = plus - Math.floor(answer);
+        if (minus == 1) {
+            return plus;
+        } else {
+            return exactAnswer;
+        }
+    }
+
+}
--- src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
+++ src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.sbrf.reboot.calculator;
 
+import com.sbrf.reboot.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,23 +8,113 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CalculatorTest {
 
     @Test
-    void getAddition() {
+    void getAdditionWhenNumbersAreInt() {
         assertEquals(9, new Calculator().getAddition(4, 5));
     }
 
     @Test
-    void getSubtraction() {
+    void getAdditionWhenNumbersAreDouble() {
+        assertEquals(5.4, new Calculator().getAddition(2.2, 3.2));
+    }
+
+    @Test
+    void getAdditionWhenNumbersAreMixed() {
+        assertEquals(5.2, new Calculator().getAddition(2, 3.2));
+    }
+
+    @Test
+    void getSubtractionWhenNumbersAreInt() {
         assertEquals(-1, new Calculator().getSubtraction(4, 5));
     }
 
     @Test
-    void getMultiplication() {
+    void getSubtractionWhenNumbersAreDouble() {
+        assertEquals(-3, new Calculator().getSubtraction(5.5, 8.5));
+    }
+
+    @Test
+    void getSubtractionWhenNumbersAreMixed() {
+        assertEquals(-3.5, new Calculator().getSubtraction(5, 8.5));
+    }
+
+    @Test
+    void getMultiplicationWhenNumbersAreInt() {
         assertEquals(20, new Calculator().getMultiplication(4, 5));
     }
 
     @Test
-    void getDivision() {
+    void getMultiplicationWhenNumbersAreDouble() {
+        assertEquals(2.4, new Calculator().getMultiplication(2.4, 1.0));
+    }
+
+    @Test
+    void getMultiplicationWhenNumbersAreMixed() {
+        assertEquals(2.4, new Calculator().getMultiplication(2, 1.2));
+    }
+
+    @Test
+    void getDivisionWhenNumbersAreInt() {
         assertEquals(3, new Calculator().getDivision(9, 3));
+    }
+
+    @Test
+    void getDivisionWhenNumbersAreDouble() {
+        assertEquals(2.5, new Calculator().getDivision(5.0, 2.0));
+    }
+
+    @Test
+    void getDivisionWhenNumbersAreMixed() {
+        assertEquals(2.5, new Calculator().getDivision(5, 2.0));
+    }
+
+    @Test
+    void getExponentiationWhenNumbersAreInt() {
+        assertEquals(8, new Calculator().getExponentiation(2, 3));
+    }
+
+    @Test
+    void getExponentiationWhenNumbersAreDouble() {
+        assertEquals(2, new Calculator().getExponentiation(4.0, 0.5));
+    }
+
+    @Test
+    void getExponentiationWhenNumbersAreMixed() {
+        assertEquals(2, new Calculator().getExponentiation(4, 0.5));
+    }
+
+    @Test
+    void getNthRootWhenNumbersAreInt() {
+        assertEquals(2, new Calculator().getNthRoot(4, 2));
+    }
+
+    @Test
+    void getNthRootWhenNumbersAreDouble() {
+        assertEquals(1.2, new Calculator().getNthRoot(1.44, 2.0));
+    }
+
+    @Test
+    void getNthRootWhenNumbersAreMixed() {
+        assertEquals(1.2, new Calculator().getNthRoot(1.44, 2));
+    }
+
+    @Test
+    void getLogarithmWhenNumbersAreInt() {
+        assertEquals(3, new Calculator().getLogarithm(8, 2));
+    }
+
+    @Test
+    void getLogarithmWhenNumbersAreIntButAnswerIsDouble() {
+        assertEquals(1.464973520717927, new Calculator().getLogarithm(5, 3));
+    }
+
+    @Test
+    void getLogarithmWhenNumbersAreDouble() {
+        assertEquals(2, new Calculator().getLogarithm(1.0201, 1.01));
+    }
+
+    @Test
+    void getLogarithmWhenNumbersAreMixed() {
+        assertEquals(2, new Calculator().getLogarithm(9, 3.0));
     }
 
     @Test
